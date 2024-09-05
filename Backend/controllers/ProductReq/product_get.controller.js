a0a46544029b4ee41a094ebb7b1d532d949ca635
Ojaschikara <ojaschikara@email.com>
2024-09-05 17:41:29 +0530

    completed

--- Backend/controllers/ProductReq/product_get.controller.js
+++ Backend/controllers/ProductReq/product_get.controller.js
@@ -1,35 +1,35 @@
-const ProductModel = require("../../models/product.model");
+// const ProductModel = require("../../models/product.model");
 
-const productGet = async (req, res) => {
-  const search = req.query.search;
-  const category = req.query.category;
+// const productGet = async (req, res) => {
+//   const search = req.query.search;
+//   const category = req.query.category;
 
-  let query = {};
+//   let query = {};
 
-  if (search) {
-    query.$or = [
-      { tags: { $regex: search, $options: "i" } },
-      { category: { $regex: search, $options: "i" } },
-      { title: { $regex: search, $options: "i" } },
-      { description: { $regex: search, $options: "i" } },
-    ];
-  }
+//   if (search) {
+//     query.$or = [
+//       { tags: { $regex: search, $options: "i" } },
+//       { category: { $regex: search, $options: "i" } },
+//       { title: { $regex: search, $options: "i" } },
+//       { description: { $regex: search, $options: "i" } },
+//     ];
+//   }
 
-  if (category) {
-    query.category = category;
-  }
+//   if (category) {
+//     query.category = category;
+//   }
 
-  try {
+//   try {
 
-    const products = await ProductModel.find(query);
+//     const products = await ProductModel.find(query);
 
-    res.status(200).send({
-      totalProducts: products.length,
-      data: products,
-    });
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-};
+//     res.status(200).send({
+//       totalProducts: products.length,
+//       data: products,
+//     });
+//   } catch (error) {
+//     res.status(500).send({ message: error.message });
+//   }
+// };
 
-module.exports = productGet;
+// module.exports = productGet;
